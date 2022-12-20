713b167f974312aef49c4241bf9aaeee3d605149
Dirceu Belém <dirceu@fourtime.com>
2022-12-20 19:16:38 -0300

    Chamando Activity

--- app/src/main/java/blip/chat/sdk/android/test/MainActivity.kt
+++ app/src/main/java/blip/chat/sdk/android/test/MainActivity.kt
@@ -1,21 +1,43 @@
 package blip.chat.sdk.android.test
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import blip.chat.sdk.android.BlipChat
 import blip.chat.sdk.android.BlipChatActivity
+import blip.chat.sdk.android.models.BlipChatModel
+import blip.chat.sdk.android.models.BlipChatStyleModel
+import blip.chat.sdk.android.models.TYPE
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var button = findViewById<Button>(R.id.button)
 
+        var model = BlipChatModel()
+        model.key = "Ym9yaXNha2l0YToyOTQwYTY5YS1hMTBjLTRjZDEtOTgwMi1lOGU1ZGFiOTMyYmE="
+        model.type = TYPE.PLAIN
+        model.hostName = "akita-mtls.ws.0mn.io"
+        model.useMtls = true
+        var style = BlipChatStyleModel()
+        style.primary = "c9c8cc"
+        style.sentBubble = "a442f5"
+        style.receivedBubble = "706f73"
+        style.background = "7b42f5"
+        style.showOwnerAvatar = true
+        style.overrideOwnerColors = true
+        style.showUserAvatar = true
+        model.style = style
+
+        var blip = BlipChat(this, model)
+
         button.setOnClickListener {
-            var intent = Intent(this, BlipChatActivity::class.java)
-            startActivity(intent)
+            startActivity(
+                BlipChatActivity.withCachedEngine(BlipChat.ENGINE_NAME).build(blip, this)
+            )
         }
 
     }
